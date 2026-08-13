96459c39bbc914529e6824eb4358493187c21d3d
Nirmal Maharjan <maharjanoptex@gmail.com>
2026-08-13 14:59:47 +0545

    update workflow

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+
   images: {
     unoptimized: true,
   },
-  basePath: "/portfolio",
-  assetPrefix: "/portfolio/",
 };
 
 export default nextConfig;
